f8981e2d8e821fb6df8e6d69f9358da7e4ed0e78
foldicintia <foldicintia2002@gmail.com>
2023-11-29 10:32:15 +0100

    jobb változat(még nem végleges)

--- feladatok.py
+++ feladatok.py
@@ -2,7 +2,7 @@ import random
 import math
 
 
-
+"""
 # Nézzük meg a lottószámok átlagát
 def lotto_szamok():
     lista = []
@@ -64,8 +64,74 @@ def kulonbseg():
     print(lista)
     kulonbseg = legnagyobb()-legkisebb()
     return kulonbseg
+"""
+# Nézzük meg a lottószámok átlagát
+def atlag2(lotto_szamok):
+    osszeg = 0
+    atlag = 0
+    db= 0
+    for i in range(0, len(lotto_szamok), 1):
+        osszeg+=lotto_szamok[i]
+        db+=1
+    atlag=osszeg/db
+    return atlag
+
+# Hány 50-nél nagyobb szám van közte? 
+def otven_felettiek2(lotto_szamok):
+    nagyobb = 0
+    for i in range(0,len(lotto_szamok),1):
+        if lotto_szamok[i] > 50:
+            nagyobb += 1
+    return nagyobb
+
+# Melyik a legnagyobb szám? 
+def legnagyobb2(lotto_szamok):
+    max = lotto_szamok[0]
+    for i in range(0,len(lotto_szamok),1):
+        if max < lotto_szamok[i]:
+            max = lotto_szamok[i]
+    return max
+
+# Hányadiknak generáltuk a a legnagyobb számot? 
+"""addig menjen amig a legnagyobb szammal egyenlo nem lesz az i?"""
+def legnagyobb_sorszama(lotto_szamok):
+    max = legnagyobb2(lotto_szamok)
+    sorszam = 1
+    for i in range(0,len(lotto_szamok),1):
+        if lotto_szamok[i] != max:
+            sorszam += 1
+    return sorszam 
+
+         
+
+# Melyik a legkisebb szám? 
+def legkisebb2(lotto_szamok):
+    min = lotto_szamok[0]
+    for i in range(0, len(lotto_szamok),1):
+        if min > lotto_szamok[i]:
+            min = lotto_szamok[i]
+    return min
+
+# A legkisebb és a legnagyobb szám közti különbség? 
+def kulonbseg2(lotto_szamok):
+    kulonbseg = legnagyobb2(lotto_szamok)-legkisebb2(lotto_szamok)
+    return kulonbseg
 
 # Kérj be a felhasználótól egy számot, és döntsd el, hogy szerepel -e a szám a húzott számok között?
+def felhasznalo_szama(lotto_szamok):
+    i = 0
+    szam: int = int(input("Adj meg egy számot 1 és 90 között!"))
+    while not (szam > 1 and szam < 90):
+        print("Nem 1 és 90 közötti számot adtál meg!")
+        szam: int = int(input("Adj meg egy számot 1 és 90 között!"))
+        i += 1
+    for i in range(0, len(lotto_szamok), 1):
+        if lotto_szamok[i] == szam:
+            return szam
+
+
+            
+
 
 #NEHEZEK
 # Kérj be a felhasználótól 5 számot, és döntsd el, hogy van-e találata? 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import feladatok
 
-lista = feladatok.lotto_szamok()
+"""lista = feladatok.lotto_szamok()
 print(lista)
 
 atlag = feladatok.atlag()
@@ -19,20 +19,32 @@ print(f"A legkisebb generált szám: {min}")
 
 kulonbseg = feladatok.kulonbseg()
 print(f"A legkisebb és a legnagyobb generált szám közötti különbség: {kulonbseg}")
+"""
+
+lotto_szamok=(5,22,14,58,66)
+
+atlag = feladatok.atlag2(lotto_szamok)
+print(f"A lottószámok átlaga: {atlag}")
 
 
+nagyobb = feladatok.otven_felettiek2(lotto_szamok)
+print(f"{nagyobb} db 50 feletti szám van a lottószámok között.")
 
 
+max = feladatok.legnagyobb2(lotto_szamok)
+print(f"A legnagyobb szám: {max}")
 
+sorszam = feladatok.legnagyobb_sorszama(lotto_szamok)
+print(f"A legnagyobb szám a/az {sorszam}. lottószám.")
 
+min = feladatok.legkisebb2(lotto_szamok)
+print(f"A legkisebb szám: {min}")
 
+kulonbseg = feladatok.kulonbseg2(lotto_szamok)
+print(f"A legkisebb és a legnagyobb szám közötti különbség: {kulonbseg}")
 
-
-
-
-
-
-
+szam = feladatok.felhasznalo_szama(lotto_szamok)
+print(f"Az általad megadott -{szam}- szám szerepel a lottószámok között.")
 
 
 
